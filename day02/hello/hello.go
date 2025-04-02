24d27e7b832706e7a8c50e2939672091ef9797c8
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2025-04-03 03:16:36 +0900

    Enhance Hello function with error handling and random message formats

--- day02/hello/hello.go
+++ day02/hello/hello.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Gladys")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
